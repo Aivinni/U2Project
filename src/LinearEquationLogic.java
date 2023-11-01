295b84ecff9d9b87a4dde6ada27089c485e548dc
aivin1 <aivinni1219@gmail.com>
2023-10-31 22:50:40 -0400

    Fin.

--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.logging.XMLFormatter;
 
 public class LinearEquationLogic {
     Scanner scan = new Scanner(System.in);
@@ -17,12 +16,12 @@ public class LinearEquationLogic {
     public void getData() {
         System.out.println("What is the first point on the graph? ");
         String point1 = scan.nextLine();
-        double x1 = Integer.parseInt(point1.substring(point1.indexOf("(") + 1, point1.indexOf(", ")));
-        double y1 = Integer.parseInt(point1.substring(point1.indexOf(", ") + 2, point1.indexOf(")")));
+        int x1 = Integer.parseInt(point1.substring(point1.indexOf("(") + 1, point1.indexOf(", ")));
+        int y1 = Integer.parseInt(point1.substring(point1.indexOf(", ") + 2, point1.indexOf(")")));
         System.out.println("What is the second point on the graph? ");
         String point2 = scan.nextLine();
-        double x2 = Integer.parseInt(point2.substring(point1.indexOf("(") + 1, point2.indexOf(",")));
-        double y2 = Integer.parseInt(point2.substring(point1.indexOf(", ") + 2, point2.indexOf(")")));
+        int x2 = Integer.parseInt(point2.substring(point2.indexOf("(") + 1, point2.indexOf(", ")));
+        int y2 = Integer.parseInt(point2.substring(point2.indexOf(", ") + 2, point2.indexOf(")")));
         equation1 = new LinearEquation(x1, y1, x2, y2);
     }
     public void printInfo() {
@@ -30,35 +29,36 @@ public class LinearEquationLogic {
             System.out.println(equation1.lineInfo());
         }
         if (equation1.errorCheck()) {
-            System.out.println("WARNING WARNING WARNING WARNING WARNING WARNING WARNING");
-            System.out.println(" WARNING WARNING WARNING WARNING WARNING WARNING WARNING");
-            System.out.println("  WARNING WARNING WARNING WARNING WARNING WARNING WARNING");
-            System.out.println("   WARNING WARNING WARNING WARNING WARNING WARNING WARNING");
-            System.out.println("    WARNING WARNING WARNING WARNING WARNING WARNING WARNING");
-            System.out.println("     WARNING WARNING WARNING WARNING WARNING WARNING WARNING");
-            System.out.println("      WARNING WARNING WARNING WARNING WARNING WARNING WARNING");
-            System.out.println("       WARNING WARNING WARNING WARNING WARNING WARNING WARNING");
-            System.out.println("WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING");
+            System.out.println("WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING");
+            System.out.println(" WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNI   WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNIN");
+            System.out.println("G WARNING WARNING WARNING WARNING WARNING WARNING WARNING WAR     WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNI");
+            System.out.println("NG WARNING WARNING WARNING WARNING WARNING WARNING WARNING W       WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARN");
+            System.out.println("ING WARNING WARNING WARNING WARNING WARNING WARNING WARNING         WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WAR");
+            System.out.println("NING WARNING WARNING WARNING WARNING WARNING WARNING WARNING       WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARN");
+            System.out.println("RNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING     WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNI");
+            System.out.println("ARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING   WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNIN");
+            System.out.println("WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING");
             System.out.println("You have attempted to divide by 0");
             System.out.println("DO NOT ATTEMPT AGAIN");
             System.out.println("Division by 0 will result in a black hole, and the inevitable destruction of humanity");
             System.out.println("YOU HAVE BEEN WARNED");
-            System.out.println("WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING");
-            System.out.println("       WARNING WARNING WARNING WARNING WARNING WARNING WARNING");
-            System.out.println("      WARNING WARNING WARNING WARNING WARNING WARNING WARNING");
-            System.out.println("     WARNING WARNING WARNING WARNING WARNING WARNING WARNING");
-            System.out.println("    WARNING WARNING WARNING WARNING WARNING WARNING WARNING");
-            System.out.println("   WARNING WARNING WARNING WARNING WARNING WARNING WARNING");
-            System.out.println("  WARNING WARNING WARNING WARNING WARNING WARNING WARNING");
-            System.out.println(" WARNING WARNING WARNING WARNING WARNING WARNING WARNING");
-            System.out.println("WARNING WARNING WARNING WARNING WARNING WARNING WARNING");
+            System.out.println("WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING");
+            System.out.println("ARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING   WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNIN");
+            System.out.println("RNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING     WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNI");
+            System.out.println("NING WARNING WARNING WARNING WARNING WARNING WARNING WARNING       WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARN");
+            System.out.println("ING WARNING WARNING WARNING WARNING WARNING WARNING WARNING         WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WAR");
+            System.out.println("NG WARNING WARNING WARNING WARNING WARNING WARNING WARNING W       WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARN");
+            System.out.println("G WARNING WARNING WARNING WARNING WARNING WARNING WARNING WAR     WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNI");
+            System.out.println(" WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNI   WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNIN");
+            System.out.println("WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING");
+            System.out.println("The Equation of this line is " + equation1.equation());
             run = false;
         }
     }
     public void valueForX() {
         double x;
         run = true;
-        while (run) {
+        while (run && !equation1.errorCheck()) {
             System.out.println("Enter a value for x: ");
             x = scan.nextDouble();
             scan.nextLine();
